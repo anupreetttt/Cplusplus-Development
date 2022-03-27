9c08e17c87beea096b39cbff75f5680f96144049
anupreetttt <anupreetpaulkar@gmail.com>
2022-03-26 20:10:34 -0500

    Linear serach

---
+++ linearsearch.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int array[], int find)
+{
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (array[i] == find){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+
+    int array[] = {2, 4, 6, 8, 10, 12, 14};
+
+    int find = 10;
+    int searchIndex = linearSearch(array, find);
+
+    cout << searchIndex << endl;
+
+}
