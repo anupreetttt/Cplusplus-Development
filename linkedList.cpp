def9435b7ff9914b2af6bb599a3480f511e1da2a
anupreetttt <anupreetpaulkar@gmail.com>
2022-03-31 01:59:19 -0500

    Insertion at the beginning of a link list

--- linkedList.cpp
+++ linkedList.cpp
@@ -19,27 +19,50 @@ void llTraversal(Node *ptr)
     }
 }
 
+Node *insertAtBeginning(Node *head, int data)
+{
+
+    Node *ptr = new Node;
+    ptr->next = head;
+    ptr->data = data;
+    return ptr;
+}
+
 int main()
 {
 
-// Link list implementaion
-    Node *head; // head is pointing towards first node
+    // Link list implementaion
+    Node *head;   // head is pointing towards first node
     Node *second; // first node is pointing towards second
-    Node *third; // second node is pointing towards third
+    Node *third;  // second node is pointing towards third
+    Node *fourth;
+    Node *inserted;
 
     head = new Node;
     second = new Node;
     third = new Node;
+    fourth = new Node;
+    inserted = new Node;
 
     head->data = 7;
     head->next = second;
+
+    // inserted->data = 6969;
+    // inserted->next = second;
 
     second->data = 213;
     second->next = third;
 
     third->data = 124;
-    third->next = NULL;
+    third->next = fourth;
+
+    fourth->data = 14;
+    fourth->next = NULL;
 
     llTraversal(head);
+    cout << "After insertion -->" << endl;
+    head = insertAtBeginning(head, 9696);
+    llTraversal(head);
+
     return 0;
 }
