6f7a3ebd88bae1bb941d9d7b42bc23ad46d59b78
anupreetttt <anupreetpaulkar@gmail.com>
2022-04-12 14:06:18 -0500

    Stacks push and pop

--- stacks.cpp
+++ stacks.cpp
@@ -1,19 +1,82 @@
 #include <iostream>
 using namespace std;
 
-class Stack{
-    public:
+class Stack
+{
+public:
     int size;
     int top;
     int *arr;
 };
 
-int main(){
+int isFull(Stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
-    Stack *s;
-    s->size = 80;
-    s->top = -1;
-    s->arr = new int;
+int isEmpty(Stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void push(Stack*ptr, int value){
+    if (isFull(ptr))
+    {
+        cout << "Stack overflow" << endl;
+    } else{
+        ptr->top++;
+        ptr->arr[ptr->top] = value;
+    }
+}
+
+int pop(Stack *ptr){
+    int val;
+    if (isEmpty(ptr))
+    {
+        cout << "Stack underflow" << endl;
+        return -1;
+    } else{
+        val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+    
+}
+
+int main()
+{
+
+    Stack *sp = new Stack;
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = new int;
+
+cout << isFull(sp) << endl;
+cout << isEmpty(sp) << endl;
+
+push(sp, 56);
+
+cout << isFull(sp) << endl;
+cout << isEmpty(sp) << endl;
+
+pop(sp);
+
+cout << isFull(sp) << endl;
+cout << isEmpty(sp) << endl;
 
     return 0;
 }
