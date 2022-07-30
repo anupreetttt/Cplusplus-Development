0fcee636af54fec391889650953f14e82bb8d4a7
Anupreet Paulkar <apaulk2@uic.edu>
2022-07-29 22:41:43 -0500

    350. Intersection of Two Arrays II

---
+++ Problem set/350. Intersection of Two Arrays II.cpp
@@ -0,0 +1,32 @@
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [4,9]
+// Explanation: [9,4] is also accepted.
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return intersect(nums2, nums1);
+        }
+
+        unordered_map<int, int> lookup;
+        for (const auto& i : nums1) {
+            ++lookup[i];
+        }
+
+        vector<int> result;
+        for (const auto& i : nums2) {
+            if (lookup[i] > 0) {
+                result.emplace_back(i);
+                --lookup[i];
+            }
+        }
+
+        return result;
+    }
+};
